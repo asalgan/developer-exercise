7b0213ec847bdf16110e2848e2addefb5477a48a
Alan Salganik <alan@babybin.com>
2016-11-17 10:27:51 -0600

    working on where

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -2,14 +2,15 @@ module Where
 
   def where(*args)
 
-    select do |x|
-      puts x
-      x.each_pair do |k, v|
-        puts k
-        puts v
-        args[k] == v
-      end
-    end
+    # select do |x|
+    #   puts x
+    #   x.each_pair do |k, v|
+    #     puts k
+    #     puts v
+    #     args[k] == v
+    #   end
+    # end
+    select { |h| h.each_pair { |k, v| h[k.to_sym] == v } }
 
   end
 
